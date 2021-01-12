a2a1a25549f67fafbe9506b5058895a0b9460398
PondJaᵀᴴ <palapon2545@gmail.com>
2021-01-13 00:28:39 +0700

    Update style 🎨
    - Updated Logo, Website's name
    - Updated font-family to Inter, Apple-System, Roboto, Kanit
    - Fixed color bug on <a> with btn class.
    //TODO -> Open Graph Meta

--- static/functions/navbar.php
+++ static/functions/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-normal fixed-top bg-white z-depth-0" id="nav" role="navigation">
-    <a class="navbar-brand" href="../home/">Grader.ga</a>
+    <a class="navbar-brand" href="../home/"><img src="../static/elements/logo/logo.png" width="32" alt="PharmMDKKU" align="center"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
