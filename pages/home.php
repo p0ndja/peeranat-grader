6d62514adb83fe426593cbd1b7ad137c3c79881a
PondJaᵀᴴ <palapon2545@gmail.com>
2021-01-15 01:43:07 +0700

    Update style

--- pages/home.php
+++ pages/home.php
@@ -3,7 +3,7 @@
         <div class="h-100 w-100 row align-items-center">
             <div class="d-none d-md-block col-md-1"></div>
             <div class="col-12 col-md-5">
-                <h1 class="font-weight-bold text-coe">Grader.ga</h1>
+                <h1 class="font-weight-bold text-coe display-4">Grader.ga</h1>
                 <h4 class="font-weight-normal">The Computer Engineering of <b class="text-nowrap">Khon Kaen University</b> Student-Made grader.</h4>
                 <a class="btn btn-coe" href="../problem/">เริ่มทำโจทย์กันเลย !</a>
                 <img src="../static/elements/index/3255466.png" class="mt-3 img-fluid w-100 d-block d-md-none">
