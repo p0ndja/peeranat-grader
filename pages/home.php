1588bab09c1038485f9e94f002bc49998e211ada
PondJaᵀᴴ <palapon2545@gmail.com>
2021-01-20 20:22:56 +0700

    Update Style 🎨

--- pages/home.php
+++ pages/home.php
@@ -3,14 +3,19 @@
         <div class="h-100 w-100 row align-items-center">
             <div class="d-none d-md-block col-md-1"></div>
             <div class="col-12 col-md-5">
-                <h1 class="font-weight-bold text-coe display-4">Grader.ga</h1>
-                <h4 class="font-weight-normal">The Computer Engineering of <b class="text-nowrap">Khon Kaen University</b> Student-Made grader.</h4>
-                <a class="btn btn-coe" href="../problem/">เริ่มทำโจทย์กันเลย !</a>
-                <img src="../static/elements/index/3255466.png" class="mt-3 img-fluid w-100 d-block d-md-none">
-                
+                <div class="bounceInDown animated">
+                    <h1 class="font-weight-bold text-coe display-4">Grader.ga</h1>
+                    <h4 class="font-weight-normal">The Computer Engineering of <b class="text-nowrap">Khon Kaen University</b><br>Student-Made grader.</h4>
+                    <a class="btn btn-coe" href="../problem/">เริ่มทำโจทย์กันเลย !</a>
+                </div>
+                <div class="fadeIn animated">
+                    <img src="../static/elements/index/3255466.png" class="mt-3 img-fluid w-100 d-block d-md-none">
+                </div>
             </div>
             <div class="col-12 col-md-6 d-none d-md-block">
-                <img src="../static/elements/index/3255466.png" class="img-fluid w-100">
+                <div class="fadeIn animated">
+                    <img src="../static/elements/index/3255466.png" class="img-fluid w-100">
+                </div>
             </div>
         </div>
     </div>
--- pages/page_loader.php
+++ pages/page_loader.php
@@ -14,7 +14,8 @@
     <?php if (isset($_GET['target']) && file_exists($_GET['target'])) include $_GET['target']; ?>
     <?php require '../static/functions/popup.php'; ?>
     <?php require '../static/functions/footer.php'; ?>
-    <div id="watermark" class="text-right text-muted">Preview Version<br><small>Some features will be changed in the final version.</small></div>
+    <!--div id="watermark" class="text-right text-danger"><small>Happy birthday Grader.ga!</small><br>♪ (｡´＿●`)ﾉ┌iiii┐ヾ(´○＿`*) ♪</div-->
+    <div id="watermark" class="text-right text-muted">Beta Version!<br><small>We're so glad to see you here!</small></div>
 </body>
 
 </html>
