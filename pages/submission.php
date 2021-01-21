495fc53514e3efb11eebb85f125057418cb14017
PondJaᵀᴴ <palapon2545@gmail.com>
2021-01-22 00:07:46 +0700

    Submission now use DataTable
    - Order by ID (descending)
    - Support searching and pagination
    - Not support 'Only my submission' yet.

--- pages/submission.php
+++ pages/submission.php
@@ -1,22 +1,25 @@
 <div class="container mb-3" style="padding-top: 88px;" id="container">
     <h1 class="display-4 font-weight-bold text-center text-coe">Submission</h1>
     <?php if (isLogin()) { ?>
-    <div class="switch switch-danger mb-1 ">
+    <div class="switch switch-danger mb-1 disabled">
         <label>
             <input type="checkbox" name="onlyme" id="onlyme">
-            <span class="lever"></span>Only My Submission
+            <span class="lever"></span>Only My Submission (BUG ;-;)
         </label>
     </div>
     <script>
         $("#onlyme").change(function() {
-            if ($(this).is(':checked'))
+            if ($(this).is(':checked')) {
                 $('tr:not(.me)').hide();
-            else
+            } else {
                 $('tr:not(.me)').show();
+            }
+            submission_table.draw();
+
         });
     </script>
     <?php } ?>
-    <table class="table table-responsive table-hover w-100 d-block d-md-table">
+    <table class="table table-responsive table-hover w-100 d-block d-md-table" id="submissionTable">
         <thead>
             <tr class="text-nowrap me">
                 <th scope="col" class="font-weight-bold text-coe">ID</th>
@@ -34,6 +37,7 @@
                     $stmt->execute();
                     $result = $stmt->get_result();
                     if ($result->num_rows > 0) {
+                        $i = 0;
                         while ($row = $result->fetch_assoc()) {
                             $me = (isLogin() && ($_SESSION['id'] == $row['user'] || isAdmin($_SESSION['id'], $conn))) ? "data-owner='true'" : "data-owner='false'";
                             $subID = $row['id'];
@@ -42,10 +46,11 @@
                             $subLang = $row['lang'];
                             $subResult = $row['result'] != 'W' ? $row['result']: 'รอผลตรวจ...';
                             $subRuntime = $row['runningtime']/1000;
-                            $subUploadtime = $row['uploadtime']; ?>
+                            $subUploadtime = $row['uploadtime']; 
+                            $i++; ?>
                             <tr style="cursor: pointer;" class='launchModal' <?php echo $me;?> id='sub<?php echo $subID;?>' onclick='javascript:;' data-toggle='modal' data-target='#modalPopup' data-title='Submission #<?php echo $subID; ?>' data-id='<?php echo $subID; ?>' data-uid='<?php echo $subUser; ?>'>
-                                <th scope='row'><?php echo $subID; ?></th>
-                                <td><?php echo $subUploadtime; ?></td>
+                                <th scope='row' data-order='<?php echo $i; ?>'><?php echo $subID; ?></th>
+                                <td data-order='<?php echo $i; ?>'><?php echo $subUploadtime; ?></td>
                                 <td><?php echo getUserdata($subUser, 'username', $conn); ?></td>
                                 <td><?php echo prob($subProb, $conn); ?></td>
                                 <td><?php echo $subLang; ?></td>
@@ -59,4 +64,12 @@
             ?>
         </tbody>
     </table>
+    <script>
+        $(document).ready(function () {
+            var submission_table = $('#submissionTable').DataTable({
+                "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "ทั้งหมด"] ]
+            });
+            $('.dataTables_length').addClass('bs-select');
+        });
+    </script>
 </div>
