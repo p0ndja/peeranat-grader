425d6de39e0a10a4460a35eb3530106652e1f99a
PondJaᵀᴴ <palapon2545@gmail.com>
2021-01-28 12:42:23 +0700

    Fixed Waiting Judgement return empty 📪

--- pages/prob_result.php
+++ pages/prob_result.php
@@ -2,7 +2,7 @@
 require_once '../static/functions/connect.php';
 $id = isset($_GET['id']) ? (int) $_GET['id'] : 1;
 $needTime = isset($_GET['time']) ? true : false;
-if ($stmt = $conn -> prepare("SELECT `submission`.`result` as result,`submission`.`score` as score,`submission`.`maxScore` as maxScore,`problem`.`score` as probScore FROM `submission` INNER JOIN `problem` ON `problem`.`id` = `submission`.`problem` WHERE id = ? LIMIT 1")) {
+if ($stmt = $conn -> prepare("SELECT `submission`.`result` as result,`submission`.`score` as score,`submission`.`maxScore` as maxScore,`problem`.`score` as probScore FROM `submission` INNER JOIN `problem` ON `problem`.`id` = `submission`.`problem` WHERE `submission`.`id` = ? LIMIT 1")) {
     $stmt->bind_param('i', $id);
     $stmt->execute();
     $result = $stmt->get_result();
