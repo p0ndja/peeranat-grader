c7e102e8afdcf03c5e781d4425b25eafcb0bb447
PondJaᵀᴴ <palapon2545@gmail.com>
2021-03-02 01:19:46 +0700

    Custom Scrollbar 📜

--- static/functions/darkmode.php
+++ static/functions/darkmode.php
@@ -5,8 +5,6 @@
         $_SESSION['dark_mode'] = false;
     } else {
         $_SESSION['dark_mode'] = true;
-        $_SESSION['swal_warning'] = "แจ้งเตือน";
-        $_SESSION['swal_warning_msg'] = "กำลังอยู่ในช่วงพัฒนา หากพบสิ่งผิดปกติโปรดแจ้ง";
     }
     back();
 ?>
