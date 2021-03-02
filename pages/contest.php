5dc1832ed0d180c18bb2e1c7d8d5b77ca2dd09b4
PondJaᵀᴴ <palapon2545@gmail.com>
2021-03-02 15:24:42 +0700

    Contest Initialize 🏁

--- pages/contest.php
+++ pages/contest.php
@@ -1,69 +1,163 @@
+<?php $incoming = false; $contest = true; $countdown = 0; ?>
+<div class="container mb-3" style="padding-top: 88px;">
+    <?php
+        if ($stmt = $conn -> prepare("SELECT * FROM `contest` WHERE CURRENT_TIMESTAMP() BETWEEN startTime AND endTime ORDER BY `id` LIMIT 1")) {
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $contest = true;
+                $cont_title = $row['title'];
+                $cont_prob = json_decode($row['problem']);
+                $probList = "(" . implode(",",$cont_prob) . ")";
+                $cont_start = $row['startTime'];
+                $cont_end = $row['endTime'];
+                $countdown = $cont_end;        
+            } else {
+                $contest = false;
+            }
+            $stmt->free_result();
+            $stmt->close();  
+        }
 
-    <?php $incoming = false; $contest = true; ?>
-    <div class="container mb-3" style="padding-top: 88px;">
-        <?php if ($contest) { ?>
-        <div align="center">
-            <h3 class="font-weight-bold text-coe">Contest Name</h3>
-            <h1 class="display-1 font-weight-bold">12:00:00</h1>
-            <p>2021/01/01 00.01 GMT+7 to 2021/12/31 23.59 GMT+7<br></p>
-        </div>
-        <table class="table table-responsive w-100 d-block d-md-table">
+        if (!$contest) {
+            if ($stmt = $conn -> prepare("SELECT * FROM `contest` WHERE CURRENT_TIMESTAMP() < startTime ORDER BY `startTime` LIMIT 1")) {
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $incoming = true;
+                    $inc_cont_title = $row['title'];
+                    $inc_cont_prob = $row['problem'];
+                    $inc_cont_start = $row['startTime'];
+                    $inc_cont_end = $row['endTime'];  
+                    $countdown = $inc_cont_end;      
+                } else {
+                    $incoming = false;
+                }
+                $stmt->free_result();
+                $stmt->close();  
+            }
+        }
+        ?>
+    <?php if ($contest) { ?>
+    <div align="center">
+        <h3 class="font-weight-bold text-coe"><?php echo $cont_title; ?></h3>
+        <h1 class="display-1 font-weight-bold" id="countdown">Loading..</h1>
+        <p><?php echo $cont_start . ' GMT+7 to ' . $cont_end . ' GMT+7'; ?></p>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-hover w-100 d-block d-md-table" id="problemTable">
             <thead>
                 <tr class="text-nowrap">
-                    <th scope="col" class="font-weight-bold text-coe">Problem ID</th>
+                    <th scope="col" class="font-weight-bold text-coe text-right">ID</th>
                     <th scope="col" class="font-weight-bold text-coe">Task</th>
                     <th scope="col" class="font-weight-bold text-coe">Rate</th>
-                    <th scope="col" class="font-weight-bold text-coe">Time Limit</th>
-                    <th scope="col" class="font-weight-bold text-coe">Memory</th>
+                    <th scope="col" class="font-weight-bold text-coe">Result</th>
                 </tr>
             </thead>
             <tbody class="text-nowrap">
-                <tr onclick="window.location='../problem/1'">
-                    <th scope="row">1</th>
-                    <td>Welcome to Grader.ga</td>
-                    <td class="text-secondary">Peaceful</td>
-                    <td>1 Second</td>
-                    <td>1 Megabyte</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>โรงแรมในฝัน</td>
-                    <td class="text-success">Easy</td>
-                    <td>1 Second</td>
-                    <td>1 Megabyte</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>ตัวเลขที่สวยงาม</td>
-                    <td class="text-warning">Normal</td>
-                    <td>1 Second</td>
-                    <td>4 Megabyte</td>
-                </tr>
-                <tr>
-                    <th scope="row">4</th>
-                    <td>พ่อสั่งน้องขอ</td>
-                    <td class="text-danger">Hard</td>
-                    <td>1.5 Second</td>
-                    <td>4 Megabyte</td>
-                </tr>
+                <?php
+                $html = "";
+                if ($stmt = $conn -> prepare("SELECT id,name,properties,codename FROM `problem` WHERE id IN $probList ORDER BY id")) {
+                    //$stmt->bind_param('s', $probList);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            $id = $row['id']; $name = $row['name']; $codename = $row['codename'];
+                            
+                            $prop = json_decode($row['properties'],true);
+                            $hide = array_key_exists("hide", $prop) ? $prop["hide"] : false;
+                            $rate = array_key_exists("rating", $prop) ? $prop["rating"] : 0;
+
+                            if (!$hide || (isLogin() && isAdmin($_SESSION['id'], $conn))) {
+                                $lastResult = isLogin() ? lastResult($_SESSION['id'], $id, $conn) : "";
+                                $html .= "<tr onclick='window.open(\"../problem/$id\")'>
+                                    <th class='text-right' scope='row'><a href=\"../problem/$id\" target=\"_blank\">$id</a></th>
+                                    <td><a href=\"../problem/$id\" target=\"_blank\">$name <span class='badge badge-coekku'>$codename</span></a></td>
+                                    <td data-order='".$rate."'>".rating($rate)."</td>
+                                    <td><code>$lastResult</code></td>
+                                </tr>";
+                            }
+                        }
+                        $stmt->free_result();
+                        $stmt->close();  
+                    }
+                    echo $html;
+                }
+                ?>
             </tbody>
         </table>
-        
-        
-        <?php } else if ($incoming) { ?>
-        <div class="center" align="center">
-            <h4>มีการแข่งขันที่กำลังจะเริ่มขึ้น...</h4>
-            <h3 class="font-weight-bold text-coe">Contest Name</h3>
-            <h1 class="display-1 font-weight-bold">12:00:00</h1>
-            <p>2021/01/01 00.01 GMT+7 to 2021/12/31 23.59 GMT+7<br><small class="text-muted">(364 Days 23 Hours 58 Minute)<small></p>
-            <a href="../scoreboard/" class="btn btn-coe">ดูประวัติการแข่งขัน</a>
-        </div>
-        <?php } else { ?>
-        <div class="center" align="center">
-            <h1 class="font-weight-bold text-coe">Contest</h1>
-            <h4>ไม่มีการแข่งขันที่กำลังจะเกิดขึ้น</h4>
-            <p>อย่าลืมฝึกทำโจทย์ด้วยแหละ !</p>
-            <a href="../scoreboard/" class="btn btn-coe">ดูประวัติการแข่งขัน</a>
-        </div>
-        <?php } ?>
     </div>
+    <script>
+        $(document).ready(function () {
+            $('#problemTable').DataTable({
+                "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "ทั้งหมด"] ],
+                'columnDefs': [ {
+                    'targets': [1,3], // column index (start from 0)
+                    'orderable': false, // set orderable false for selected columns
+                }]
+            });
+            $('.dataTables_length').addClass('bs-select');
+        });
+    </script>
+
+
+    <?php } else if ($incoming) { ?>
+    <div class="center" align="center">
+        <h4>มีการแข่งขันที่กำลังจะเริ่มขึ้น...</h4>
+        <h3 class="font-weight-bold text-coe"><?php echo $inc_cont_title; ?></h3>
+        <h1 class="display-1 font-weight-bold" id="countdown">Loading..</h1>
+        <p><?php echo $inc_cont_start . ' GMT+7 to ' . $inc_cont_end . ' GMT+7'; ?></p>
+        <a href="../scoreboard/" class="btn btn-coe">ดูประวัติการแข่งขัน</a>
+    </div>
+    <?php } else { ?>
+    <div class="center" align="center">
+        <h1 class="font-weight-bold text-coe">Contest</h1>
+        <h4>ไม่มีการแข่งขันที่กำลังจะเกิดขึ้น</h4>
+        <p>อย่าลืมฝึกทำโจทย์ด้วยแหละ !</p>
+        <a href="../scoreboard/" class="btn btn-coe">ดูประวัติการแข่งขัน</a>
+    </div>
+    <?php } ?>
+</div>
+<script>
+    // Set the date we're counting down to
+    var countDownDate = new Date("<?php echo $countdown; ?>").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        var stackMessage = "";
+        if (days) {
+            stackMessage = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        } else {
+            if (hours < 10) hours = "0" + hours;
+            if (minutes < 10) minutes = "0" + minutes;
+            if (seconds < 10) seconds = "0" + seconds;
+            stackMessage += hours + ":" + minutes + ":" + seconds;
+        }
+
+        document.getElementById("countdown").innerHTML = stackMessage;
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+            location.reload();
+        }
+    }, 1000);
+</script>
