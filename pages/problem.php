35dcaa92e1d0f6964c0f3f9ff9350d1b88c54dba
PondJaᵀᴴ <palapon2545@gmail.com>
2021-03-15 23:21:15 +0700

    Add Indicator on Hidden Problem 👁

--- pages/problem.php
+++ pages/problem.php
@@ -26,11 +26,14 @@
                             $hide = array_key_exists("hide", $prop) ? $prop["hide"] : false;
                             $rate = array_key_exists("rating", $prop) ? $prop["rating"] : 0;
 
+                            $hideMessage = "";
+                            if ($hide) $hideMessage = "<span class='badge badge-danger'>ซ่อน</span>";
+
                             if (!$hide || (isLogin() && isAdmin($_SESSION['id'], $conn))) {
                                 $lastResult = isLogin() ? lastResult($_SESSION['id'], $id, $conn) : "";
                                 $html .= "<tr onclick='window.open(\"../problem/$id\")'>
                                     <th class='text-right' scope='row'><a href=\"../problem/$id\" target=\"_blank\">$id</a></th>
-                                    <td><a href=\"../problem/$id\" target=\"_blank\">$name <span class='badge badge-coekku'>$codename</span></a></td>
+                                    <td><a href=\"../problem/$id\" target=\"_blank\">$name <span class='badge badge-coekku'>$codename</span></a> $hideMessage</td>
                                     <td data-order='".$rate."'>".rating($rate)."</td>
                                     <td><code>$lastResult</code></td>
                                 </tr>";
