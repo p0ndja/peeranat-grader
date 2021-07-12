5b254741c44bb77f01b95594337432f4ab5a770e
PondJaᵀᴴ <palapon2545@gmail.com>
2021-07-12 12:05:17 +0700

    Fixed donation page

--- pages/donation.php
+++ pages/donation.php
@@ -1,68 +1,68 @@
-<div class="homepage">
-    <div class="container h-100 w-100">
-        <div class="h-100 w-100 row align-items-center">
-            <div class="col-12 col-md-8">
-                <h1 class="font-weight-bold text-center text-coekku">Donation</h1>
-                <div class="d-flex justify-content-center"><img src="../static/elements/promptpay.png" class="z-depth-1 mb-3" width="300" /></div>
-                <h5 class="text-center font-weight-bold">Palapon Soontornpas / พลภณ สุนทรภาส</h5>
-                <p class="text-center">
-                    <b>Promptpay / TrueWallet</b> : <code>090-8508007</code><br>
-                    <b>SCB</b> : <code>551-442288-3</code><br>
-                    <b>KBank</b> : <code>084-3-24454-8</code> [ไม่ใช่เบอร์โทรศัพท์!]<br>
-                    <a id="donateSubmit" class="btn btn-coekku">ส่งเอกสารการโอนเงินโดเนท</a>
-                    <script>
-                        $("#donateSubmit").on('click', function() {
-                            window.open('./submit','popUpWindow','height=200,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
-                        });
-                    </script>
-                </p>
+<div class="container mb-3" style="padding-top: 88px; min-height: 100vh !important;" id="container">
+    <h1 class="display-4 font-weight-bold text-center text-coekku">Donation</h1>
+    <div class="row">
+        <div class="col-12 col-md-8">
+            <div class="d-none d-md-block" style="padding-top: 8vh;"></div>
+            <div class="d-flex justify-content-center"><img src="https://s3.p0nd.ga/file/400d5/kbankqr.jpg" class="mb-3" width="300" /></div>
+            <h5 class="text-center font-weight-bold">Palapon Soontornpas / พลภณ สุนทรภาส</h5>
+            <p class="text-center">
+                <b>KBank</b> : <code style="color: green;">084-3-24454-8</code> <small class="text-muted">*ไม่ใช่เบอร์โทรศัพท์</small><br>
+                <b>SCB</b> : <code style="color: purple;">551-442288-3</code><br>
+                <b>Promptpay / TrueWallet</b> : <code style="color: blue;">090-8508007</code><br>
+                <center><a id="donateSubmit" class="btn btn-coekku btn-md">ส่งเอกสารการโอนเงินโดเนท</a></center>
+                <script>
+                    $("#donateSubmit").on('click', function() {
+                        window.open('./submit','popUpWindow','height=200,width=500,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes');
+                    });
+                </script>
+            </p>
+        </div>
+        <div class="col-12 col-md-4">
+        <div class="d-none d-md-block" style="padding-top: 5vh;"></div>
+            <div class="table-responsive">
+            <h4 class="font-weight-bold text-center text-coekku">Donator</h4>
+            <p class="text-center text-muted"><small>ทุกคนที่บริจาคจะได้รับ <text class="rainbow font-weight-bold">ชื่อสีรุ้ง</text> (ถาวร)</small></p>
+                <table class="table table-sm table-hover w-100" id="submissionTable" style="max-height: 60vh;">
+                    <thead>
+                        <tr class="text-nowrap me">
+                            <th scope="col" class="font-weight-bold text-coekku">User</th>
+                            <th scope="col" class="font-weight-bold text-coekku">Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-nowrap">
+                        <?php
+                            $json = json_decode(file_get_contents("http://api.11th.studio/p0ndja/donation_grader"), true);
+                            $amount = 0;
+                            foreach ($json as $j) {
+                                $time = strtotime($j['timestamp']); $month = date('m', $time);
+                                $curMonth = date('m', time());
+                                if ($month == $curMonth) {
+                                    echo "<tr><th scope='row'>".$j['name']."</th><td>".$j['value']." ฿</td></tr>";
+                                    $amount += $j['value'];
+                                }
+                            }
+                        ?>
+                    </tbody>
+                </table>
             </div>
-            <div class="col-12 col-md-4">
-                <div class="table-responsive">
-                <h4 class="font-weight-bold text-center text-coekku">Donator</h4>
-                <p class="text-center text-muted"><small>ทุกคนที่บริจาคจะได้รับ <text class="rainbow font-weight-bold">ชื่อสีรุ้ง</text> (ถาวร)</small></p>
-                    <table class="table table-sm table-hover w-100" id="submissionTable">
-                        <thead>
-                            <tr class="text-nowrap me">
-                                <th scope="col" class="font-weight-bold text-coekku">User</th>
-                                <th scope="col" class="font-weight-bold text-coekku">Amount</th>
-                            </tr>
-                        </thead>
-                        <tbody class="text-nowrap">
-                            <?php
-                                $json = json_decode(file_get_contents("http://api.11th.studio/p0ndja/donation_grader"), true);
-                                $amount = 0;
-                                foreach ($json as $j) {
-                                    $time = strtotime($j['timestamp']); $month = date('m', $time);
-                                    $curMonth = date('m', time());
-                                    if ($month == $curMonth) {
-                                        echo "<tr><th scope='row'>".$j['name']."</th><td>".$j['value']." ฿</td></tr>";
-                                        $amount += $j['value'];
-                                    }
-                                }                                
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
-                <?php   $val = ($amount / 150)*100;
-                        $valMsg = $val > 100 ? "<text class='font-weight-bold rainbow'>".number_format((float) $val, 2, '.', '')."%</text>" : number_format((float) $val, 2, '.', '') . "%"; ?>
-                <div class="progress">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated bg-coekku" role="progressbar" style="width: <?php echo $val;?>%" aria-valuenow="<?php echo $val;?>" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <small><?php echo $amount; ?>/150 THB (<?php echo $valMsg; ?>)</small>
-                <?php
-                    $f = file_get_contents("http://api.11th.studio/p0ndja/invoice.txt"); 
-                    $f = explode("\n", $f);
-                    if (!empty($f)) {
-                        echo "<hr>ประวัติการชำระค่า Server<br>";
-                        foreach($f as $l) {
-                            $i = explode(" ", $l);
-                            if (!empty($i[0]))
-                                echo "<a href='$i[0]' target='_blank'><span class='badge badge-coekku'>$i[1]</span></a> ";
-                        }
+            <?php   $val = ($amount / 150)*100;
+                    $valMsg = $val > 100 ? "<text class='font-weight-bold rainbow'>".number_format((float) $val, 2, '.', '')."%</text>" : number_format((float) $val, 2, '.', '') . "%"; ?>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated bg-coekku" role="progressbar" style="width: <?php echo $val;?>%" aria-valuenow="<?php echo $val;?>" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <small><?php echo $amount; ?>/150 THB (<?php echo $valMsg; ?>)</small>
+            <?php
+                $f = file_get_contents("http://api.11th.studio/p0ndja/invoice.txt"); 
+                $f = explode("\n", $f);
+                if (!empty($f)) {
+                    echo "<hr>ประวัติการชำระค่า Server<br>";
+                    foreach($f as $l) {
+                        $i = explode(" ", $l);
+                        if (!empty($i[0]))
+                            echo "<a href='$i[0]' target='_blank'><span class='badge badge-coekku'>$i[1]</span></a> ";
                     }
-                ?>
-            </div>
+                }
+            ?>
         </div>
     </div>
 </div>
