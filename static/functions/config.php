f21e8e5c35b7ba3b121d8ea2bfc91ee71159f85c
PondJaᵀᴴ <palapon2545@gmail.com>
2021-08-08 22:21:30 +0700

    *

--- static/functions/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db = array(
-        "hostname" => "pondja.thddns.net:4416",
-        "username" => "graderga",
-        "password" => "8db!#yYvK]8Lw6F|37wz:UwU",
-        "table" => "graderga"
-    );
-?>
