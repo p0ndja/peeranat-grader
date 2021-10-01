2818b27c4c7fda0de458b59f43287177c09c1bf9
PondJaᵀᴴ <palapon2545@gmail.com>
2021-10-02 01:47:40 +0700

    Added change password confirmation.

--- pages/profile.php
+++ pages/profile.php
@@ -12,7 +12,7 @@
 ?>
 <div class="container" style="padding-top: 88px;">
     <div class="container mb-3" id="container">
-    <form method="POST" action="../pages/profile_save.php" enctype="multipart/form-data">
+    <form method="POST" action="../pages/profile_save.php" enctype="multipart/form-data" id="userFormEdit">
         <input type="hidden" name="id" value="<?php echo $user->getID(); ?>"/>
         <div class="row">
                 <div class="col-12 col-md-4">
@@ -38,13 +38,13 @@
                                         class="fas fa-info-circle"></i></h4>
                                 <hr>
                                 <!-- name -->
-                                <div class="md-form input-group">
+                                <div class="md-form input-group mt-0 mb-0">
                                     <div class="input-group-prepend">
-                                        <span class="input-group-text md-addon">ชื่อที่แสดง</span>
+                                        <span class="input-group-text md-addon font-weight-bold">ชื่อที่แสดง</span>
                                     </div>
                                     <input type="text" class="form-control mr-sm-3" id="name" name="name"
                                         placeholder="<?php echo $user->getName(); ?>"
-                                        value="<?php echo $user->getName(); ?>">
+                                        value="<?php echo $user->getName(); ?>" disabled>
                                 </div>
                                 <!-- name -->
                                 <!-- Security Zone -->
@@ -52,9 +52,9 @@
                                 </h4>
                                 <hr>
                                 <!-- Email -->
-                                <div class="md-form input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text md-addon">อีเมล</span>
+                                <div class="md-form input-group mt-0 mb-0">
+                                    <div class="input-group-prepend mt-0 mb-0">
+                                        <span class="input-group-text md-addon font-weight-bold">อีเมล</span>
                                     </div>
                                     <input type="hidden" id="real_email" name="real_email" value="<?php echo $user->getEmail(); ?>">
                                     <input type="text" class="form-control mr-sm-3" id="email" name="email"
@@ -63,15 +63,24 @@
                                 </div>
                                 <!-- Email -->
                                 <!-- Password -->
-                                <div class="md-form input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text md-addon">รหัสผ่าน</span>
+                                <div class="md-form input-group mt-0 mb-0">
+                                    <div class="input-group-prepend mb-0">
+                                        <span class="input-group-text md-addon font-weight-bold">เปลี่ยนรหัสผ่าน</span>
                                     </div>
-                                    <input type="text" class="form-control mr-sm-3" id="password" name="password"
-                                        placeholder="พิมพ์รหัสผ่านเพื่อตั้งรหัสผ่านใหม่... (การเว้นว่างจะถือว่าใช้รหัสผ่านเดิม)"
-                                        value="">
                                 </div>
-
+                                <div class="md-form input-group mb-0 mt-0">
+                                    <div class="input-group-prepend mb-0 mt-0">
+                                        <span class="input-group-text md-addon mb-0 mt-0 ml-5">รหัสผ่านใหม่</span>
+                                    </div>
+                                    <input type="password" class="form-control mr-sm-3" id="password" name="password" value="">
+                                </div>
+                                <div class="md-form input-group mb-0 mt-0">
+                                    <div class="input-group-prepend mb-0 mt-0">
+                                        <span class="input-group-text md-addon mb-0 mt-0">รหัสผ่านใหม่อีกครั้ง</span>
+                                    </div>
+                                    <input type="password" class="form-control mr-sm-3" id="newPassword" name="newPassword" value="">
+                                </div>
+                                <small id="al" class="text-danger text-center" style="display: none;">Password doesn't match</small>
                                 <!-- Password -->
                                 <!-- Security Zone -->
                         </div>
@@ -176,5 +185,43 @@ $(document).ready(function () {
                 ];
             }
         });
+    });
+</script>
+<script>
+    var pw = document.getElementById("password");
+    var pwc = document.getElementById("newPassword");
+    var al = document.getElementById("al");
+    var validPassword = true;
+    pwc.addEventListener("change", function() {
+        if (pw.value === "") {    
+            pw.classList.remove("invalid");
+            pwc.classList.remove("invalid");
+            pw.classList.remove("valid");
+            pwc.classList.remove("valid");
+            al.style.display = "none";
+            validPassword = true;
+        } else {
+            if (pw.value != pwc.value) {
+                pw.classList.add("invalid");
+                pwc.classList.add("invalid");
+                pw.classList.remove("valid");
+                pwc.classList.remove("valid");
+                al.style.display = "block";
+                validPassword = false;
+            } else {
+                pw.classList.add("valid");
+                pwc.classList.add("valid");
+                pw.classList.remove("invalid");
+                pwc.classList.remove("invalid");
+                al.style.display = "none";
+                validPassword = true;
+            }
+        }
+    });
+    document.querySelector("#userEditForm").addEventListener("submit", function(event) {
+        if (!validPassword) {
+            event.preventDefault();
+            swal("Oops","Please check your password and try again", "error");
+        }
     });
 </script>
