ce59e1ae5bbfe352d73a33ed44f4d49ad44eca7b
PondJaᵀᴴ <palapon2545@gmail.com>
2022-10-14 10:18:54 +0700

    Update doc.php

--- pages/doc.php
+++ pages/doc.php
@@ -1,12 +1,5 @@
 <?php
     $id = $_GET['id'];
-<<<<<<< HEAD
-    $codename = $_GET['codename'];
-    //print_r("../file/judge/prob/$id/$codename*.pdf");
-    $file = glob("../file/judge/prob/$id/*.pdf");
-    //print_r($file);
-    if ($file) {
-=======
     $files = glob("../file/judge/prob/$id/*.pdf");
     $latest_ctime = 0;
     $latest_filename = null;
@@ -19,7 +12,6 @@
             }
     }
     if ($latest_filename != null) {
->>>>>>> e157ae7e05b0be9d022abac747e99184a90e78a9
         header("Content-type: application/pdf");
         readfile($latest_filename);
     } else {
